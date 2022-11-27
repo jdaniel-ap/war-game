9d4e383515d880af5d08056ba3340c968eb36fd3
Jose Daniel <jdaniell.93@gmail.com>
2022-11-27 18:53:27 -0300

    Initial Commit

---
+++ wars/warsApp.swift
@@ -0,0 +1,17 @@
+//
+//  warsApp.swift
+//  wars
+//
+//  Created by Jose Daniel on 27/11/22.
+//
+
+import SwiftUI
+
+@main
+struct warsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
